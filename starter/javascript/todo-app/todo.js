38541c27a7e377547c993adfd12088c642c6f46e
Kauan Polydoro <kauan.2812@hotmail.com>
2020-08-13 10:36:03 -0300

    Iniciando a aplicação

---
+++ starter/javascript/todo-app/todo.js
@@ -0,0 +1,11 @@
+//Obtendo os elementos que serão modificados via JavaScript
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer café',
+    'Estudar JavaScript',
+    'Finalizar o módulo todo-app'
+];
+
