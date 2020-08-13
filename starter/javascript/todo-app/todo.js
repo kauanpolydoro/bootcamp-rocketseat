5430748dfd613be288a430a32cfc618931d03585
Kauan Polydoro <kauan.2812@hotmail.com>
2020-08-13 10:44:08 -0300

    Criando TODO

--- starter/javascript/todo-app/todo.js
+++ starter/javascript/todo-app/todo.js
@@ -11,6 +11,8 @@ var todos = [
 
 function renderTodos() {
 
+    listElement.innerHTML = '';
+
     for (todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
@@ -22,3 +24,13 @@ function renderTodos() {
 }
 
 renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+}
+
+buttonElement.onclick = addTodo;
