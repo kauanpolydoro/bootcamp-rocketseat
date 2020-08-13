4b0d3ce0a96900213a9afa60dd5c977e9388ea25
Kauan Polydoro <kauan.2812@hotmail.com>
2020-08-13 17:53:13 -0300

    Promises

--- starter/javascript/js-assincrono/main.js
+++ starter/javascript/js-assincrono/main.js
@@ -1,3 +1,4 @@
+/*Ajax
 var xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'http://api.github.com/users/kauanpolydoro');
@@ -8,4 +9,32 @@ xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
         console.log(JSON.parse(xhr.responseText));
     }
+}*/
+
+//Promisse
+
+var myPromise = function () {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'http://api.github.com/users/kauanpolydoro');
+        xhr.send(null);
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject('Erro na requisição');
+                }
+            }
+        }
+    })
 }
+
+myPromise()
+    .then(function (resolve) {
+        console.log(resolve);
+    })
+    .catch(function (error) {
+        console.warn(error);
+    });
