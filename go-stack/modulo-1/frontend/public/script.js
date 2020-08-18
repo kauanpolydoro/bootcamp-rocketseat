08224ac818f948809860e8bb7161aa3d1f0a2f7a
Kauan Polydoro <kauan.2812@hotmail.com>
2020-08-18 17:52:34 -0300

    Configurando Babel

---
+++ go-stack/modulo-1/frontend/public/script.js
@@ -0,0 +1,6 @@
+/**
+ * Babel: Converter (transpilar) código do React para um código que o browser entenda.
+ * Webpack: Converte cada tipo de arquivo (.js, .css, .png) de uma maneira diferente
+ * 
+ * Loaders: Utilizado pelo webpack para converter os arquivos, babel-loader, css-loader, image-loader
+ */
