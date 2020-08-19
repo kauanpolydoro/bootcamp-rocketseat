036e127fb232e7216f627212a852e4421538b5ca
Kauan Polydoro <kauan.2812@hotmail.com>
2020-08-19 14:14:33 -0300

    Propriedades

--- go-stack/modulo-1/frontend/src/App.js
+++ go-stack/modulo-1/frontend/src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 
 import Header from './components/Header'
 
+/** Conceitos do React:
+ * Componente
+ * Propriedade
+ * Estado
+ */
+
 function App() {
     return (
         <>
-            <Header />
-            <Header />
-            <Header />
-            <Header />
-            <Header />
+            <Header title="Homepage">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projetcs</li>
+                </ul>
+            </Header>
+            <Header title="Projects">
+                <ul>
+                    <li>Login</li>
+                </ul>
+            </Header>
         </>
     );
 }
