cb17c7b2e954c423ef2aee6c9581198a781f531f
Kauan Polydoro <kauan.2812@hotmail.com>
2020-08-20 14:37:27 -0300

    Listando projetos da API

--- go-stack/modulo-1/frontend/src/App.js
+++ go-stack/modulo-1/frontend/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api.js';
 
 import './App.css';
-import backgroundImage from './assets/background.jpg';
 
 import Header from './components/Header'
 
@@ -21,7 +21,13 @@ function App() {
 
      */
 
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('/projects').then(response => {
+            setProjects(response.data);
+        })
+    }, []);
 
     function handleAddProject() {
         setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -30,9 +36,8 @@ function App() {
     return (
         <>
             <Header title="Projects" />
-            <img width={500} src={backgroundImage} alt="background" />
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
