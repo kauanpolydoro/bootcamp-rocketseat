553bcb91b3655b1f3c9bf1cd86ea3ed12163cbbb
Kauan Polydoro <kauan.2812@hotmail.com>
2020-08-20 14:51:45 -0300

    Cadastrando projetos pela API

--- go-stack/modulo-1/frontend/src/App.js
+++ go-stack/modulo-1/frontend/src/App.js
@@ -29,8 +29,17 @@ function App() {
         })
     }, []);
 
-    function handleAddProject() {
-        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    async function handleAddProject() {
+        //setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Kauan Polydoro"
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
     }
 
     return (
