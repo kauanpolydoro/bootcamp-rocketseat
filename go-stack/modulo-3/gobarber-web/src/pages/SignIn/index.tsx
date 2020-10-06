41ad17e5925d4bfc3fb144626aaa7d53ac9188df
Kauan Poyldoro <kauan.2812@hotmail.com>
2020-10-06 13:11:55 -0300

    Finalizando a página de login

--- go-stack/modulo-3/gobarber-web/src/pages/SignIn/index.tsx
+++ go-stack/modulo-3/gobarber-web/src/pages/SignIn/index.tsx
@@ -9,19 +9,13 @@ const SignIn: React.FC = () => (
   <Container>
     <Content>
       <img src={logoImg} alt="GoBarber" />
-
       <form>
         <h1>Faça seu logon</h1>
-
         <input placeholder="E-mail" />
-
         <input type="password" placeholder="Senha" />
-
         <button type="submit">Entrar</button>
-
         <a href="forgot">Esqueci minha senha</a>
       </form>
-
       <a href="login">
         <FiLogIn />
         Criar conta
