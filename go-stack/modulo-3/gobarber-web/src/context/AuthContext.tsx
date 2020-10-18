417e600035f79c14064326499b8012ec4fe43a42
Kauan Poyldoro <kauan.2812@hotmail.com>
2020-10-18 13:34:07 -0300

    Criando hook useAuth

--- go-stack/modulo-3/gobarber-web/src/context/AuthContext.tsx
+++ go-stack/modulo-3/gobarber-web/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useState, useContext } from 'react';
 import api from '../services/api';
 
 interface IAuthState {
@@ -16,7 +16,7 @@ interface IAuthContext {
   signIn(credentials: ISignInCredentials): Promise<void>;
 }
 
-export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
+const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<IAuthState>(() => {
@@ -47,3 +47,13 @@ export const AuthProvider: React.FC = ({ children }) => {
     </AuthContext.Provider>
   );
 };
+
+export function useAuth(): IAuthContext {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+
+  return context;
+}
