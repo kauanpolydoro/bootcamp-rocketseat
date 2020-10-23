0969af1af28dd577be4d80d23628b87755331c2c
Kauan Poyldoro <kauan.2812@hotmail.com>
2020-10-23 10:55:06 -0300

    Rotas autenticadas

---
+++ go-stack/modulo-3/gobarber-web/src/routes/Route.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Route as ReactDOMRoute, RouteProps, Redirect } from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface IRouteProps extends RouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<IRouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
