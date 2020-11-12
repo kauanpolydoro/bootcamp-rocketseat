2d4591d0ef9e249de5d7ff2c094964c0754ef5bb
Kauan Polydoro <kauan.2812@hotmail.com>
2020-11-12 18:54:16 -0300

    Autenticação de usuários

---
+++ go-stack/modulo-3/appgobarber/src/hooks/auth.tsx
@@ -0,0 +1,77 @@
+import React, {
+  createContext, useCallback, useState, useContext, useEffect,
+} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '../services/api';
+
+interface IAuthState {
+  token: string;
+  user: Record<string, unknown>;
+}
+
+interface ISignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface IAuthContext {
+  user: Record<string, unknown>;
+  signIn(credentials: ISignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<IAuthContext>({} as IAuthContext);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<IAuthState>({} as IAuthState);
+
+  useEffect(() => {
+    async function loadStorageData(): Promise<void> {
+      const [token, user] = await AsyncStorage.multiGet([
+        '@GoBarber:token',
+        '@GoBarber:user',
+      ]);
+
+      if (token[1] && user[1]) {
+        setData({ token: token[1], user: JSON.parse(user[1]) });
+      }
+    }
+
+    loadStorageData();
+  }, []);
+
+  const signOut = useCallback(async () => {
+    await AsyncStorage.multiRemove(['@GoBarber:token', '@GoBarber:user']);
+
+    setData({} as IAuthState);
+  }, []);
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', { email, password });
+
+    const { token, user } = response.data;
+
+    await AsyncStorage.multiSet([
+      ['@GoBarber:token', token],
+      ['@GoBarber:user', JSON.stringify(user)],
+    ]);
+
+    setData({ token, user });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth(): IAuthContext {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+
+  return context;
+}
--- go-stack/modulo-3/appgobarber/src/pages/SignIn/index.tsx
+++ go-stack/modulo-3/appgobarber/src/pages/SignIn/index.tsx
@@ -10,6 +10,7 @@ import Icon from 'react-native-vector-icons/Feather';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import getValidationErrors from '../../utils/getValidationErrors';
+import { useAuth } from '../../hooks/auth';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -35,6 +36,8 @@ const SignIn: React.FC = () => {
   const passwordInputRef = useRef<TextInput>(null);
   const navigation = useNavigation();
 
+  const { signIn } = useAuth();
+
   const handleSignIn = useCallback(
     async (data: ISignInFormData) => {
       try {
@@ -51,9 +54,7 @@ const SignIn: React.FC = () => {
           abortEarly: false,
         });
 
-        // await signIn({ email: data.email, password: data.password });
-
-        // history.push('/dashboard');
+        await signIn({ email: data.email, password: data.password });
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
@@ -66,7 +67,7 @@ const SignIn: React.FC = () => {
         Alert.alert('Erro na autenticação', 'Ocorreu um erro ao fazer login, cheque as credenciais.');
       }
     },
-    [],
+    [signIn],
   );
 
   return (
