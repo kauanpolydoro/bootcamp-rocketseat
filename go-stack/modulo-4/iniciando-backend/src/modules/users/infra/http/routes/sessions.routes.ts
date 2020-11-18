8864b91042bd1f93709e28a734f2a7a011247c2f
Kauan Poyldoro <kauan.2812@hotmail.com>
2020-11-18 09:12:53 -0300

    Refatorando módulo de usuários

--- go-stack/modulo-4/iniciando-backend/src/modules/users/infra/http/routes/sessions.routes.ts
+++ go-stack/modulo-4/iniciando-backend/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import UsersRepository from '../../typeorm/repositories/UsersRepositories';
 
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
     const { email, password } = request.body;
+    const usersRepository = new UsersRepository();
 
-    const authenticateUserService = new AuthenticateUserService();
+    const authenticateUserService = new AuthenticateUserService(
+        usersRepository,
+    );
 
     const { user, token } = await authenticateUserService.excute({
         email,
--- go-stack/modulo-4/iniciando-backend/src/modules/users/infra/http/routes/users.routes.ts
+++ go-stack/modulo-4/iniciando-backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -4,16 +4,17 @@ import uploadConfig from '@config/upload';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-
+import UsersRepository from '../../typeorm/repositories/UsersRepositories';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
+    const usersRepository = new UsersRepository();
     const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+    const createUser = new CreateUserService(usersRepository);
 
     const user = await createUser.execute({ name, email, password });
 
@@ -27,7 +28,8 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-        const updateUserAvatar = new UpdateUserAvatarService();
+        const usersRepository = new UsersRepository();
+        const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
 
         const user = await updateUserAvatar.execute({
             user_id: request.user.id,
