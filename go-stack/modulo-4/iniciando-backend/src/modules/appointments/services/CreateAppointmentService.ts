d2be36f442b4845c7fcf53bd68c75b4a758beb4e
Kauan Poyldoro <kauan.2812@hotmail.com>
2020-11-18 07:58:06 -0300

    Reescrevendo Repositórios

--- go-stack/modulo-4/iniciando-backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ go-stack/modulo-4/iniciando-backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -6,13 +6,16 @@ import AppError from '@shared/errors/AppError';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
 
-interface Request {
+interface IRequest {
     provider_id: string;
     date: Date;
 }
 
 class CreateAppointmentService {
-    public async execute({ date, provider_id }: Request): Promise<Appointment> {
+    public async execute({
+        date,
+        provider_id,
+    }: IRequest): Promise<Appointment> {
         const appointmentsRepository = getCustomRepository(
             AppointmentsRepository,
         );
@@ -27,12 +30,10 @@ class CreateAppointmentService {
             throw new AppError('This appointment is already booked');
         }
 
-        const appointment = appointmentsRepository.create({
+        const appointment = await appointmentsRepository.create({
             provider_id,
             date: appointmentDate,
         });
-
-        await appointmentsRepository.save(appointment);
 
         return appointment;
     }
