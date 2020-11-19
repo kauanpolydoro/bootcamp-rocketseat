0dc7579b295b9ccc36f0dcad3214a290b8a71ec4
Kauan Poyldoro <kauan.2812@hotmail.com>
2020-11-19 07:59:13 -0300

    Testes de agendamento

--- go-stack/modulo-4/iniciando-backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ go-stack/modulo-4/iniciando-backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,6 @@
-import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import AppError from '@shared/errors/AppError';
 
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
@@ -18,7 +19,24 @@ describe('CreateAppointment', () => {
         expect(appointment.provider_id).toBe('1');
     });
 
-    it('should not be able to create two appointments in the same date', () => {
-        expect(1 + 2).toBe(3);
+    it('should not be able to create two appointments in the same date', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointmentDate = new Date(2020, 4, 10, 11);
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '2',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '3',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
     });
 });
