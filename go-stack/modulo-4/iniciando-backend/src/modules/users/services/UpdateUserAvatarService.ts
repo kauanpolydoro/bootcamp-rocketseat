d3d8e47509b1a0b347ba6c820f60672482e15d71
Kauan Polydoro <kauan.2812@hotmail.com>
2020-11-19 23:58:30 -0300

    Provider de storage

--- go-stack/modulo-4/iniciando-backend/src/modules/users/services/UpdateUserAvatarService.ts
+++ go-stack/modulo-4/iniciando-backend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,12 +1,10 @@
-import path from 'path';
-import fs from 'fs';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-import uploadConfig from '@config/upload';
 
-import User from '../infra/typeorm/entities/User';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
+import User from '../infra/typeorm/entities/User';
 
 interface IRequest {
     user_id: string;
@@ -18,6 +16,9 @@ class UpdateUserAvatarService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('StorageProvider')
+        private storageProvider: IStorageProvider,
     ) {}
 
     public async execute({ user_id, avatarFileName }: IRequest): Promise<User> {
@@ -31,27 +32,14 @@ class UpdateUserAvatarService {
         }
 
         if (user.avatar) {
-            // Deleta o avatar anterior
-
-            const userAvatarFilePath = path.join(
-                uploadConfig.directory,
-                user.avatar,
-            );
-
-            const userAvatarFileExists = await fs.promises.stat(
-                userAvatarFilePath,
-            );
-
-            if (userAvatarFileExists) {
-                await fs.promises.unlink(userAvatarFilePath);
-            }
+            await this.storageProvider.deleteFile(user.avatar);
         }
 
-        user.avatar = avatarFileName;
+        const filename = await this.storageProvider.saveFile(avatarFileName);
+
+        user.avatar = filename;
 
         await this.usersRepository.save(user);
-
-        delete user.password;
 
         return user;
     }
