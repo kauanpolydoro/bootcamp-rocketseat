4357117f3ac3ce2641f7135ba7b51801e58de0b2
Kauan Polydoro <kauan.2812@hotmail.com>
2020-11-22 08:44:33 -0300

    Atualização do perfil

---
+++ go-stack/modulo-4/iniciando-backend/src/modules/users/services/UpdateProfileService.spec.ts
@@ -0,0 +1,125 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import UpdateProfileService from './UpdateProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let updateProfile: UpdateProfileService;
+
+describe('UpdateProfile', () => {
+    beforeEach(() => {
+        fakeUsersRepository = new FakeUsersRepository();
+        fakeHashProvider = new FakeHashProvider();
+
+        updateProfile = new UpdateProfileService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
+    });
+
+    it('should be able to update the profile', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        const updatedUser = await updateProfile.execute({
+            user_id: user.id,
+            name: 'Foo Bar',
+            email: 'foobar@example.com',
+        });
+
+        expect(updatedUser.name).toBe('Foo Bar');
+        expect(updatedUser.email).toBe('foobar@example.com');
+    });
+
+    it('should not be able to update the email to an already exists one', async () => {
+        await fakeUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456',
+        });
+
+        const user = await fakeUsersRepository.create({
+            name: 'Foo Bar',
+            email: 'foobar@example.com',
+            password: '123456',
+        });
+
+        await expect(
+            updateProfile.execute({
+                user_id: user.id,
+                name: 'Foo Bar',
+                email: 'johndoe@example.com',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should be able to update the password', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'Foo Bar',
+            email: 'foobar@example.com',
+            password: '123456',
+        });
+
+        const updatedUser = await updateProfile.execute({
+            user_id: user.id,
+            name: 'Foo Bar',
+            email: 'johndoe@example.com',
+            old_password: '123456',
+            password: '654321',
+        });
+
+        await expect(updatedUser.password).toBe('654321');
+    });
+
+    it('should not be able to update the password without the old password', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'Foo Bar',
+            email: 'foobar@example.com',
+            password: '123456',
+        });
+
+        await expect(
+            updateProfile.execute({
+                user_id: user.id,
+                name: 'Foo Bar',
+                email: 'johndoe@example.com',
+                password: '654321',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to update the password with a wrong old password', async () => {
+        const user = await fakeUsersRepository.create({
+            name: 'Foo Bar',
+            email: 'foobar@example.com',
+            password: '123456',
+        });
+
+        await expect(
+            updateProfile.execute({
+                user_id: user.id,
+                name: 'Foo Bar',
+                email: 'johndoe@example.com',
+                old_password: 'wrong-old-password',
+                password: '654321',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+
+    it('should not be able to update the password of a non-existing user', async () => {
+        await expect(
+            updateProfile.execute({
+                user_id: 'non-existing-user',
+                name: 'Foo Bar',
+                email: 'johndoe@example.com',
+                old_password: 'wrong-old-password',
+                password: '654321',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
+});
