cb226edb66d823dcbdf399f9702d8e004ac52385
Kauan Polydoro <kauan.2812@hotmail.com>
2020-11-29 19:58:38 -0300

    Refatorando testes

--- go-stack/modulo-4/backend/src/modules/appointments/services/ListProvidersService.ts
+++ go-stack/modulo-4/backend/src/modules/appointments/services/ListProvidersService.ts
@@ -24,8 +24,6 @@ class ListProvidersService {
                 except_user_id: user_id,
             });
 
-            console.log('Query realizada no banco de dados');
-
             await this.cacheProvider.save(`providers-list:${user_id}`, users);
         }
 
