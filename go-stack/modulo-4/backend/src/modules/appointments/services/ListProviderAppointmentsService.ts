f8c5cb6f5feae86c10a40a7af10a5b0c3d3d7e3b
Kauan Polydoro <kauan.2812@hotmail.com>
2020-11-29 19:35:10 -0300

    Cache de agendamentos

--- go-stack/modulo-4/backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ go-stack/modulo-4/backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -27,15 +27,19 @@ class ListProviderAppointmentsService {
         month,
         year,
     }: IRequest): Promise<Appointment[]> {
-        const cacheData = await this.cacheProvider.recover('teste');
+        const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
 
-        console.log({ cacheData });
-
-        const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
-            { provider_id, day, month, year },
+        let appointments = await this.cacheProvider.recover<Appointment[]>(
+            cacheKey,
         );
 
-        // await this.cacheProvider.save('teste', 'teste');
+        if (!appointments) {
+            appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+                { provider_id, day, month, year },
+            );
+
+            await this.cacheProvider.save(cacheKey, appointments);
+        }
 
         return appointments;
     }
