fbe90d6a4f3b951e964a310cb4fcd602d0105d58
Kauan Polydoro <kauan.2812@hotmail.com>
2021-03-08 19:16:26 -0300

    Adicionando o Nodemon na aplicação

--- ignite/node-js/fundamentos-node-js/src/index.js
+++ ignite/node-js/fundamentos-node-js/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ msg: 'Hello World' })
+  return response.json({ msg: 'Hello World' });
 })
 
 app.listen(3333, () => console.log('✔ Listen on port 3333'));
