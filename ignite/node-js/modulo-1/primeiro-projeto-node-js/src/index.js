4b1f966988a5f75bd24977ef03f32839cc952785
Kauan Polydoro <kauan.2812@hotmail.com>
2021-03-09 20:03:39 -0300

    Cadastro de conta

--- ignite/node-js/modulo-1/primeiro-projeto-node-js/src/index.js
+++ ignite/node-js/modulo-1/primeiro-projeto-node-js/src/index.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const { v4 } = require('uuid');
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+  const id = v4();
+
+  customers.push({
+    cpf,
+    name, id,
+    statement: []
+  });
+
+  return response.status(201).send();
+
+});
 
 app.listen(3333, () => {
   console.log('✔ Listen on port 3333');
