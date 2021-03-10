2e4b64484a25fb5f66b4d8972c95a1e74d777a76
Kauan Poyldoro <kauan.2812@hotmail.com>
2021-03-10 08:32:41 -0300

    Listando extrato

--- ignite/node-js/modulo-1/primeiro-projeto-node-js/src/index.js
+++ ignite/node-js/modulo-1/primeiro-projeto-node-js/src/index.js
@@ -7,12 +7,14 @@ app.use(express.json());
 
 const customers = [];
 
-app.post('/account', (request, response) => {
+app.post('/account', (request, response) =>
+{
   const { cpf, name } = request.body;
 
   const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
 
-  if (customerAlreadyExists) {
+  if (customerAlreadyExists)
+  {
     return response.status(400).json({ error: 'Customer already exists' });
   }
 
@@ -27,6 +29,28 @@ app.post('/account', (request, response) => {
 
 });
 
-app.listen(3333, () => {
+app.get('/statement/:cpf', (request, response) =>
+{
+
+  const { cpf } = request.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer)
+  {
+    return response.status(404).json({ msg: 'Customer not found' });
+  }
+
+  if (!customer.statement.length)
+  {
+    return response.status(204).send();
+  }
+
+  return response.status(200).json(customer.statement);
+
+})
+
+app.listen(3333, () =>
+{
   console.log('✔ Listen on port 3333');
 });
