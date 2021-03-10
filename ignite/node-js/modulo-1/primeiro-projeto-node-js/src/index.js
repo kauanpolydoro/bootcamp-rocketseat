6832ce0a123d50641afcf183cc4afec8e1986b14
Kauan Polydoro <kauan.2812@hotmail.com>
2021-03-10 20:12:05 -0300

    Criando depósito na conta

--- ignite/node-js/modulo-1/primeiro-projeto-node-js/src/index.js
+++ ignite/node-js/modulo-1/primeiro-projeto-node-js/src/index.js
@@ -54,6 +54,23 @@ app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
 
 })
 
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+})
+
 app.listen(3333, () => {
   console.log('✔ Listen on port 3333');
 });
