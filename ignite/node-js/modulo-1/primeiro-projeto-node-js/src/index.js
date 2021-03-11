ccec1a37b12d02235dd7f68bb826bd76d583cf96
Kauan Poyldoro <kauan.2812@hotmail.com>
2021-03-11 13:18:46 -0300

    Remover conta

--- ignite/node-js/modulo-1/primeiro-projeto-node-js/src/index.js
+++ ignite/node-js/modulo-1/primeiro-projeto-node-js/src/index.js
@@ -161,6 +161,23 @@ app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
   return response.json(customer);
 })
 
+app.delete('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const {customer } = request;
+  customers.splice(customer, 1);
+
+  return response.status(204).send();
+})
+
+app.get('/balance', verifyIfExistsAccountCPF, (request, response) => {
+
+  const {customer} = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.json(balance);
+
+})
+
 app.listen(3333, () =>
 {
   console.log('✔ Listen on port 3333');
