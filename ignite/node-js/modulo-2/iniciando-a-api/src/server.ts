3f6ca843b2213c294feb2a7b50ef94b0cf196bc2
Kauan Polydoro <kauan.2812@hotmail.com>
2021-03-16 19:21:48 -0300

    Criando categoria

--- ignite/node-js/modulo-2/iniciando-a-api/src/server.ts
+++ ignite/node-js/modulo-2/iniciando-a-api/src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
-app.get("/", (request, response) => {
-    response.json({ message: "Hello World" });
-});
+app.use(express.json());
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("✔ Server started on port 3333!"));
