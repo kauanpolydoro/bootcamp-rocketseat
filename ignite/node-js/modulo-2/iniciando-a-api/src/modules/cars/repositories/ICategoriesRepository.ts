413cfb1abbefcb8a53c4455aba1245e46c49d6ff
Kauan Polydoro <kauan.2812@hotmail.com>
2021-03-17 19:59:54 -0300

    Criando repositório de especificação

--- ignite/node-js/modulo-2/iniciando-a-api/src/modules/cars/repositories/ICategoriesRepository.ts
+++ ignite/node-js/modulo-2/iniciando-a-api/src/modules/cars/repositories/ICategoriesRepository.ts
@@ -6,9 +6,9 @@ interface ICreateCategoryDTO {
 }
 
 interface ICategoriesRepository {
+    create(data: ICreateCategoryDTO): void;
     findByName(name: string): Category;
     list(): Category[];
-    create(data: ICreateCategoryDTO): void;
 }
 
 export { ICategoriesRepository, ICreateCategoryDTO };
