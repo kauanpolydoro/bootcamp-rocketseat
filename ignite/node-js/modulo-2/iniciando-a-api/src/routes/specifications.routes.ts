5a79d16762f9b76a7d60af747bc7f3fad02a519e
Kauan Poyldoro <kauan.2812@hotmail.com>
2021-03-18 13:33:24 -0300

    Criando use case de especificação

--- ignite/node-js/modulo-2/iniciando-a-api/src/routes/specifications.routes.ts
+++ ignite/node-js/modulo-2/iniciando-a-api/src/routes/specifications.routes.ts
@@ -1,21 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationRouter = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 specificationRouter.post("/", (request, response) => {
-    const { name, description } = request.body;
-
-    const createSpecificationService = new CreateSpecificationService(
-        specificationsRepository
-    );
-
-    createSpecificationService.execute({ name, description });
-
-    return response.status(201).send();
+    createSpecificationController.handle(request, response);
 });
 
 export { specificationRouter };
