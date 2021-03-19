a7fc4bbb60f7c4fc6f4d30eab9f201d4892c1a83
Kauan Polydoro <kauan.2812@hotmail.com>
2021-03-19 18:50:13 -0300

    Adicionando desafio-introducao-ao-solid

---
+++ ignite/node-js/desafios/desafio-introducao-ao-solid/src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,25 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  name: string;
+  email: string;
+}
+
+class CreateUserUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ email, name }: IRequest): User {
+    const findUser = this.usersRepository.findByEmail(email);
+
+    if (findUser) {
+      throw new Error("This email is already taken");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
+  }
+}
+
+export { CreateUserUseCase };
