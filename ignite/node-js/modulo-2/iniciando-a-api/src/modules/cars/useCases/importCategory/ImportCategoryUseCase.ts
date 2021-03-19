63003aec87184199ec5f12f234ba9b5cd2f527cf
Kauan Polydoro <kauan.2812@hotmail.com>
2021-03-18 21:32:13 -0300

    Conhecendo o conceito de stream

--- ignite/node-js/modulo-2/iniciando-a-api/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ ignite/node-js/modulo-2/iniciando-a-api/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,10 +1,21 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 class ImportCategoryUseCase {
     constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    execute(file: any): void {
-        console.log(file);
+    execute(file: Express.Multer.File): void {
+        const stream = fs.createReadStream(file.path);
+
+        const parseFile = csvParse();
+
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
     }
 }
 
