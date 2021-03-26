1d763cd3fcbc323fae407b1a07a35f93df6fa626
Kauan Poyldoro <kauan.2812@hotmail.com>
2021-03-26 10:06:22 -0300

    Criando token de usuário

--- ignite/node-js/modulo-3/continuando-a-aplicação/src/routes/index.ts
+++ ignite/node-js/modulo-3/continuando-a-aplicação/src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationRouter } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
+router.use(authenticateRoutes);
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationRouter);
 router.use("/users", usersRoutes);
