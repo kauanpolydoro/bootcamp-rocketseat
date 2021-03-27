cd35d779c394d4cdfc69c0acbc1b00e5d4fe4ed6
Kauan Polydoro <kauan.2812@hotmail.com>
2021-03-27 17:57:00 -0300

    Tratamento de exceções

--- ignite/node-js/modulo-3/continuando-a-aplicação/src/server.ts
+++ ignite/node-js/modulo-3/continuando-a-aplicação/src/server.ts
@@ -1,10 +1,12 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
 import "./shared/container";
 
+import { AppError } from "./errors/AppErrors";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -13,5 +15,17 @@ const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+        status: "Error",
+        message: "Internal server error",
+        error: err.message,
+    });
+});
 
 app.listen(3333, () => console.log("✔ Server started on port 3333!"));
