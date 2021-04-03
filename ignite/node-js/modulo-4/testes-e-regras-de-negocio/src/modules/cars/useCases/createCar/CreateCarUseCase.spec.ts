fff67c8552ce2d48c90a938a7550c801d0f6bcee
Kauan Polydoro <kauan.2812@hotmail.com>
2021-04-02 21:24:19 -0300

    TDD na prática

---
+++ ignite/node-js/modulo-4/testes-e-regras-de-negocio/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,25 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepository: CarsRepositoryInMemory;
+
+describe("Create Car", () => {
+    beforeEach(() => {
+        carsRepository = new CarsRepositoryInMemory();
+        createCarUseCase = new CreateCarUseCase(carsRepository);
+    });
+
+    it("should be able to create a new car", async () => {
+        await createCarUseCase.execute({
+            name: "Car name",
+            description: "Car Description",
+            daily_rate: 100,
+            license_plate: "ABC-1234",
+            fine_amount: 60,
+            brand: "Brand",
+            category_id: "category",
+        });
+    });
+});
