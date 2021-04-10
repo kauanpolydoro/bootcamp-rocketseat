404f894c358cc793651ff9df8599ec1b74424ebd
Kauan Polydoro <kauan.2812@hotmail.com>
2021-04-10 13:34:10 -0300

    Continuação do cadastro de aluguel

--- ignite/node-js/modulo-4/testes-e-regras-de-negocio/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ ignite/node-js/modulo-4/testes-e-regras-de-negocio/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
@@ -13,11 +14,46 @@ describe("Create Rental", () => {
         );
     });
 
-    it("should be able to create a new Rental", async () => {
-        await createRentalUseCase.execute({
+    it("should be able to create a new rental", async () => {
+        const rental = await createRentalUseCase.execute({
             user_id: "12345",
             car_id: "123123123",
             expected_return_date: new Date(),
         });
+
+        expect(rental).toHaveProperty("id");
+        expect(rental).toHaveProperty("start_date");
+    });
+
+    it("should not be able to create two rentals to the same user", () => {
+        expect(async () => {
+            await createRentalUseCase.execute({
+                user_id: "1",
+                car_id: "1",
+                expected_return_date: new Date(),
+            });
+
+            await createRentalUseCase.execute({
+                user_id: "1",
+                car_id: "2",
+                expected_return_date: new Date(),
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("should not be able to create two rentals to the same car", () => {
+        expect(async () => {
+            await createRentalUseCase.execute({
+                user_id: "1",
+                car_id: "1",
+                expected_return_date: new Date(),
+            });
+
+            await createRentalUseCase.execute({
+                user_id: "2",
+                car_id: "1",
+                expected_return_date: new Date(),
+            });
+        }).rejects.toBeInstanceOf(AppError);
     });
 });
