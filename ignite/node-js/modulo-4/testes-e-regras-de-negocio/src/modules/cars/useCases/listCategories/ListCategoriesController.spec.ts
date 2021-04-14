4d0a4ce002ef37b9385dc2f42d439fd900cbf8e1
Kauan Poyldoro <kauan.2812@hotmail.com>
2021-04-14 08:43:46 -0300

    Criando teste de listagem de categorias

--- ignite/node-js/modulo-4/testes-e-regras-de-negocio/src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ ignite/node-js/modulo-4/testes-e-regras-de-negocio/src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -8,7 +8,7 @@ import createConnection from "@shared/infra/typeorm";
 
 let connection: Connection;
 
-describe("Create Category Controller", () => {
+describe("List Categories Controller", () => {
     beforeAll(async () => {
         connection = await createConnection();
         await connection.runMigrations();
@@ -45,7 +45,7 @@ describe("Create Category Controller", () => {
         await connection.close();
     });
 
-    it("should be able to create a new category", async () => {
+    it("should be able to list all categories", async () => {
         const responseToken = await request(app).post("/sessions").send({
             email: "admin@rentx.com.br",
             password: "admin",
@@ -53,7 +53,7 @@ describe("Create Category Controller", () => {
 
         const { token } = responseToken.body;
 
-        const response = await request(app)
+        await request(app)
             .post("/categories")
             .send({
                 name: "Test category",
@@ -63,27 +63,11 @@ describe("Create Category Controller", () => {
                 Authorization: `Bearer ${token}`,
             });
 
-        expect(response.status).toBe(201);
-    });
+        const response = await request(app).get("/categories");
 
-    it("should not be able to create a category that already exists", async () => {
-        const responseToken = await request(app).post("/sessions").send({
-            email: "admin@rentx.com.br",
-            password: "admin",
-        });
-
-        const { token } = responseToken.body;
-
-        const response = await request(app)
-            .post("/categories")
-            .send({
-                name: "Test category",
-                description: "Test description",
-            })
-            .set({
-                Authorization: `Bearer ${token}`,
-            });
-
-        expect(response.status).toBe(400);
+        expect(response.status).toBe(200);
+        expect(response.body.length).toBe(1);
+        expect(response.body[0]).toHaveProperty("id");
+        expect(response.body[0].name).toEqual("Test category");
     });
 });
