82ee03416c7bb514322eae9899bc81c7507ced51
Kauan Polydoro <kauan.2812@hotmail.com>
2021-04-17 19:01:11 -0300

    Controller de devolução de carro

--- ignite/node-js/modulo-5/trabalhando-com-refresh-token-e-email/src/shared/infra/http/routes/rental.routes.ts
+++ ignite/node-js/modulo-5/trabalhando-com-refresh-token-e-email/src/shared/infra/http/routes/rental.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 const rentalsRouter = Router();
 
 rentalsRouter.post("/", ensureAuthenticated, createRentalController.handle);
+rentalsRouter.post(
+    "/devolution/:id",
+    ensureAuthenticated,
+    devolutionRentalController.handle
+);
 
 export { rentalsRouter };
