3df396770f5246cca94b591eb9f53ae17cd50f10
Kauan Poyldoro <kauan.2812@hotmail.com>
2021-04-19 11:13:19 -0300

    Refatorando Autenticação do usuário

---
+++ ignite/node-js/modulo-5/trabalhando-com-refresh-token-e-email/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "a7e071b3de48cec1dd24de6cbe6c7bf1",
+    secret_refresh_token: "35204d8000600ee32b78d914499d430b",
+    expires_in_token: "15m",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_in_days: 30,
+};
