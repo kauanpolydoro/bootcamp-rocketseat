b5d9b7d4e292b7080349178058b920bb7f2bcc09
Kauan Polydoro <kauan.2812@hotmail.com>
2021-04-20 19:55:25 -0300

    Controller refresh token

--- ignite/node-js/modulo-5/trabalhando-com-refresh-token-e-email/src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
+++ ignite/node-js/modulo-5/trabalhando-com-refresh-token-e-email/src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -5,11 +5,16 @@ import { RefreshTokenUseCase } from "./RefreshTokenUseCase";
 
 class RefreshTokenController {
     async handle(request: Request, response: Response): Promise<Response> {
+        const token =
+            request.body.token ||
+            request.headers["x-access-token"] ||
+            request.query.token;
+
         const refreshTokenUseCase = container.resolve(RefreshTokenUseCase);
 
-        await refreshTokenUseCase.execute();
+        const refresh_token = await refreshTokenUseCase.execute(token);
 
-        return response.status(200).send();
+        return response.status(200).json(refresh_token);
     }
 }
 
