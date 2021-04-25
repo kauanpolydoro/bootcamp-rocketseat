e129bc475410ba9cf44af16e3774db4914182ed2
Kauan Polydoro <kauan.2812@hotmail.com>
2021-04-25 19:38:55 -0300

    Estilizando modal

--- ignite/react/modulo-2/dtmoney/src/components/NewTransactionModal/index.tsx
+++ ignite/react/modulo-2/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg';
 import { Container } from './styles';
 
 interface NewTransactionModalProps {
@@ -14,15 +15,24 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       overlayClassName='react-modal-overlay'
       className='react-modal-content'
     >
+
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+
       <Container>
         <h2>Cadastrar transação</h2>
 
-        <input placeholder="Título"/>
+        <input placeholder="Título" />
 
-        <input type="number" placeholder="Valor"/>
+        <input type="number" placeholder="Valor" />
 
-        <input placeholder="Categoria"/>
-        
+        <input placeholder="Categoria" />
+
         <button type="submit">
           Cadastrar
         </button>
