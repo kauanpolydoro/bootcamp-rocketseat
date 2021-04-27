73eee275071db826319f18859b93319cb3d174f4
Kauan Poyldoro <kauan.2812@hotmail.com>
2021-04-27 08:01:22 -0300

    Cores dos botões

--- ignite/react/modulo-2/dtmoney/src/components/NewTransactionModal/styles.ts
+++ ignite/react/modulo-2/dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 const Container = styled.form`
 
@@ -60,7 +60,13 @@ const TransactionTypeContainer = styled.div`
 `;
 
 interface RadioBoxProps {
-  isActive: boolean
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33cc95',
+  red: '#e52e4d'
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -69,7 +75,9 @@ height: 4rem;
 border: 1px solid #d7d7d7;
 border-radius: 0.25rem;
 
-background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
+background: ${(props) => props.isActive
+    ? transparentize(0.8, colors[props.activeColor])
+    : 'transparent'};
 
 display: flex;
 align-items: center;
