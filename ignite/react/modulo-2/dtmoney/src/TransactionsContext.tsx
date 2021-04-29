f1a954bf44d6aa8a58cb1a6a49a236e542d980ed
Kauan Polydoro <kauan.2812@hotmail.com>
2021-04-29 19:59:49 -0300

    Finalizando inserção

--- ignite/react/modulo-2/dtmoney/src/TransactionsContext.tsx
+++ ignite/react/modulo-2/dtmoney/src/TransactionsContext.tsx
@@ -18,7 +18,7 @@ type TransactionsInput = Omit<Transaction, 'id' | 'createdAt'>;
 
 type TransactionsContextData = {
   transactions: Transaction[],
-  createTransaction: (transaction: TransactionsInput) => void;
+  createTransaction: (transaction: TransactionsInput) => Promise<void>;
 }
 
 interface TransactionsProviderProps {
@@ -34,8 +34,15 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionsInput) {
-    api.post('/transactions', transaction);
+  async function createTransaction(transactionInput: TransactionsInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date()
+    });
+
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
   }
 
   return (
