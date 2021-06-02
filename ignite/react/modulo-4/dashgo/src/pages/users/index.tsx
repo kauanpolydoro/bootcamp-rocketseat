509551493b7f5a8522cc4eae2e62b8a09267c30a
Kauan Polydoro <kauan.2812@hotmail.com>
2021-06-02 19:52:24 -0300

    Página lista de usuários

---
+++ ignite/react/modulo-4/dashgo/src/pages/users/index.tsx
@@ -0,0 +1,66 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Siderbar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Th px="6" color="gray.300" w="8">
+                <Checkbox colorScheme="pink" />
+              </Th>
+              <Th>Usuário</Th>
+              <Th>Data de cadastro</Th>
+              <Th w="8"/>
+            </Thead>
+            <Tbody>
+              <Td px="6">
+                <Checkbox colorScheme="pink" />
+              </Td>
+              <Td>
+                <Box>
+                  <Text fontWeight="bold">Kauan Polydoro</Text>
+                  <Text fontSize="sm" color="gray.300">kauan.polydoro@email.com</Text>
+                </Box>
+              </Td>
+              <Td>04 de Abril, 2021</Td>
+              <Td>
+                <Button
+                  as="a"
+                  size="sm"
+                  fontSize="sm"
+                  colorScheme="purple"
+                  leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                >
+                  Editar
+                </Button>
+              </Td>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
