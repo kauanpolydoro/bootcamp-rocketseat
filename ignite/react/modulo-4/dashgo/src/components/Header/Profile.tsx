bddf76ed5e1790dff1cc90ac1c924edd94ac9c3b
Kauan Polydoro <kauan.2812@hotmail.com>
2021-06-07 21:40:55 -0300

    Header responsivo

--- ignite/react/modulo-4/dashgo/src/components/Header/Profile.tsx
+++ ignite/react/modulo-4/dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = false }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Kauan Polydoro</Text>
-        <Text color="gray.300" fontSize="small">
-          kauan.polydoro@teste.com
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Kauan Polydoro</Text>
+          <Text color="gray.300" fontSize="small">
+            kauan.polydoro@teste.com
             </Text>
-      </Box>
+        </Box>
+      )}
 
       <Avatar size="md" name="Kauan Polydoro" src="https://github.com/kauanpolydoro.png" />
     </Flex>
