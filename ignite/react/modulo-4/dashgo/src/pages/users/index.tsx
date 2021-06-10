8b6916a37f0b21851a52cbfe27b1d0a9880de46b
Kauan Polydoro <kauan.2812@hotmail.com>
2021-06-10 19:13:34 -0300

    Listagem responsiva

--- ignite/react/modulo-4/dashgo/src/pages/users/index.tsx
+++ ignite/react/modulo-4/dashgo/src/pages/users/index.tsx
@@ -1,10 +1,16 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  });
+
   return (
     <Box>
       <Header />
@@ -29,15 +35,15 @@ export default function UserList() {
 
           <Table colorScheme="whiteAlpha">
             <Thead>
-              <Th px="6" color="gray.300" w="8">
+              <Th px={["4", "4", "6"]} color="gray.300" w="8">
                 <Checkbox colorScheme="pink" />
               </Th>
               <Th>Usuário</Th>
-              <Th>Data de cadastro</Th>
-              <Th w="8"/>
+              {isWideVersion && <Th>Data de cadastro</Th>}
+              <Th w="8" />
             </Thead>
             <Tbody>
-              <Td px="6">
+              <Td px={["4", "4", "6"]}>
                 <Checkbox colorScheme="pink" />
               </Td>
               <Td>
@@ -46,7 +52,7 @@ export default function UserList() {
                   <Text fontSize="sm" color="gray.300">kauan.polydoro@email.com</Text>
                 </Box>
               </Td>
-              <Td>04 de Abril, 2021</Td>
+              {isWideVersion && <Td>04 de Abril, 2021</Td>}
               <Td>
                 <Button
                   as="a"
