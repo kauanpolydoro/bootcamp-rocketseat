d29900d7f9e07ccdab342c37149bb34b7fe382ae
Kauan Polydoro <kauan.2812@hotmail.com>
2021-07-04 18:28:51 -0300

    Validando autenticação (Server)

--- ignite/react/modulo-4/nextauth/context/AuthContext.tsx
+++ ignite/react/modulo-4/nextauth/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from 'react'
-import { api } from '../services/api';
+import { api } from '../services/apiClient';
 import { setCookie, parseCookies, destroyCookie } from 'nookies';
 import Router from 'next/router';
 
--- ignite/react/modulo-4/nextauth/pages/dashboard.tsx
+++ ignite/react/modulo-4/nextauth/pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../context/AuthContext"
-import { api } from "../services/api";
+import { setupAPIClient } from "../services/api";
+import { api } from '../services/apiClient';
+import { withSSRAuth } from "../utils/withSSRAuth copy";
 
 export default function Dashboard() {
 
@@ -16,3 +18,14 @@ export default function Dashboard() {
     <h1>Dashboard: {user?.email}</h1>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx);
+  const response = await apiClient.get('/me');
+
+  console.log(response.data);
+
+  return {
+    props: {}
+  }
+});
